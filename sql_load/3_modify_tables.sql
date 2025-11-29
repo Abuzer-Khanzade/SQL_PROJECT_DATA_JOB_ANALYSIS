4ab0b6eafe2fe4699dd9b9268b226a8597a852c7
Abuzer Khanzade <mohammedabuzerk@gmail.com>
2025-11-29 21:21:11 +0000

    data updated

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -26,17 +26,17 @@ NOTE: If you are having issues with permissions. And you get error:
 -- NOTE: This has been updated from the video to fix issues with encoding
 
 COPY company_dim
-FROM 'E:\SQL VS\csv_files\company_dim.csv'
+FROM 'E:\SQL VS\star_schema_files\company_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_dim
-FROM 'E:\SQL VS\csv_files\skills_dim.csv'
+FROM 'E:\SQL VS\star_schema_files\skills_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY job_postings_fact
-FROM 'E:\SQL VS\csv_files\job_postings_fact.csv'
+FROM 'E:\SQL VS\star_schema_files\job_postings_fact.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_job_dim
-FROM 'E:\SQL VS\csv_files\skills_job_dim.csv'
+FROM 'E:\SQL VS\star_schema_files\skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
